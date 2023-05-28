d3423103d766ae8f2d07458fe1cdcc2032079318
romeusorionaet <romeusoares14569@gmail.com>
2023-05-27 21:13:47 -0300

    Improving the description about me

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -11,27 +11,28 @@ export function About() {
             className='z-10 bg-rgba-2 rounded-lg p-4 flex flex-col gap-4'>
 
             <p>
-              Eu sou Romeu soares, tenho 24 anos e sou Web Developer Frontend júnior, com experiência nas principais ferramentas
-              atual no mercado. Sou apaixonado pela área, e sempre estou em busca de novas oportunidades 
-              para aprender e crescer.
+              Eu sou <span className='text-cyan-200'>Romeu soares de souto</span>, tenho 24 anos e sou Web Developer Frontend júnior, com experiência nas 
+              principais ferramentas atual no mercado. Sou apaixonado pela área, e sempre estou em busca 
+              de novas oportunidades para aprender e crescer.
             </p>
 
             <p>
               Resumindo sobre como consegui meus conhecimentos na área.
               Para estágio inícial da minha jornada concluí um curso de web design moderno
-              com conceitos básicos sobre HTML, CSS e JavaScript. Após isso iniciei e 
-              concluí com sucesso o programa Explorer da empresa de educação em tecnologia Rocketseat.
-              Nela consegui definir meus objetivos e propósitos planejando minha trajetória. 
-              Avancei meus conhecimentos nos conceitos básicos que me permitiu realizar projetos 
-              mais complexos. Aprendi a montar e consumir uma API RESTful e utilizar banco de dados. 
-              Tudo isso realizando projetos pessoais simulando o mercado.
+              com conceitos básicos sobre <i>HTML, CSS e JavaScript.</i> Após isso iniciei e 
+              concluí com sucesso o programa <strong>Explorer</strong> da empresa de educação em 
+              tecnologia <strong>Rocketseat</strong>. Nela consegui definir meus objetivos e propósitos planejando 
+              minha trajetória. Avancei meus conhecimentos nos conceitos básicos que me permitiu 
+              realizar projetos mais complexos. Aprendí a montar e consumir uma API RESTful e utilizar 
+              banco de dados. Tudo isso realizando projetos simulando o mercado.
             </p>
 
             <p>  
               Isso me permitiu encontrar soluções e aprender a utilizar novas ferramentas 
-              atuais como por exemplo a utilização do TypeScript, Nextjs e Tailwindcss.
-              E não paro por aqui, continuo aprendendo, me atualizando para está sempre apto a 
-              novos desafios.
+              atuais como por exemplo a utilização do <i>TypeScript, Nextjs e Tailwindcss.</i>
+              Atualmente estou dando continuidade aos estudos ainda na <strong>Rocketseat</strong>, agora
+              em outro programa de ensino mais avançado e especializado no mercado,
+              programa <strong>Ignite</strong>.
             </p>
 
           </div>
